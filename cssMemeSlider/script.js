8f3e92d902c12d70b6ad2647344f6bab4f69a0b2
Arseniy <krutoi.arseniy@gmail.com>
2024-09-15 17:23:51 +0300

    fix: change index.html(Sun, Sep 15, 2024 17:23:45 PM)

--- cssMemeSlider/script.js
+++ cssMemeSlider/script.js
@@ -1,4 +1,4 @@
-const button = document.querySelectorAll('.button')
+/* const button = document.querySelectorAll('.button')
 const buttonOne = document.querySelector('.button-one');
 const buttonTwo = document.querySelector('.button-two');
 const buttonThree = document.querySelector('.button-three');
@@ -18,37 +18,20 @@ const carouselImg = document.querySelector('.carousel-img');
 buttonOne.addEventListener('click', () => {
     carouselImg.src = pictures[0]
     text.textContent = 'What!?'
-    // carousel.style.transform = `translateX(-${index * 100}%)`;
-    carousel.style.transition = `transform 1s`; 
 })
 
 buttonTwo.addEventListener('click', () => {
     carouselImg.src = pictures[1]
     text.textContent = 'How I cool'
-    // carousel.style.transform = `translateX(-${index * 100}%)`;
-    carousel.style.transition = `transform 1s`; 
 })
 
 buttonThree.addEventListener('click', () =>{
     carouselImg.src = pictures[2]
     text.textContent = 'I`m always'
-    // carousel.style.transform = `translateX(-${index * 100}%)`;
-    carousel.style.transition = `transform 1s`; 
 })
 
 buttonFour.addEventListener('click', () =>{
     carouselImg.src = pictures[3]
     text.textContent = 'smart smart'
-    // carousel.style.transform = `translateX(-${index * 100}%)`;
-    carousel.style.transition = `transform 1s`; 
-})
-
-// const buttonIndex = null;
-
-/* button.forEach((el) => {
-    el.addEventListener('click', () => {
-        el.classList.toggle('active')
-    })
-
 })
  */
