e0f4a61a1aa5eff074fa46af9e99383636c784ac
Arseniy <krutoi.arseniy@gmail.com>
2024-10-31 18:41:36 +0300

    refactor: delete in css(Thu, Oct 31,2024 18:40:05 PM)

--- cssMemeSlider/script.js
+++ cssMemeSlider/script.js
@@ -1,37 +1,36 @@
-/* const button = document.querySelectorAll('.button')
-const buttonOne = document.querySelector('.button-one');
-const buttonTwo = document.querySelector('.button-two');
-const buttonThree = document.querySelector('.button-three');
-const buttonFour = document.querySelector('.button-four');
-const carousel = document.querySelector('.carousel');
-const text = document.querySelector('.text');
+const button = document.querySelectorAll(".button");
+const buttonOne = document.querySelector(".button-one");
+const buttonTwo = document.querySelector(".button-two");
+const buttonThree = document.querySelector(".button-three");
+const buttonFour = document.querySelector(".button-four");
+const carousel = document.querySelector(".carousel");
+const text = document.querySelector(".text");
 index = 0;
 const pictures = [
-    './pictures/funny-picture.png',
-    './pictures/funny-picture-2.jpg',
-    './pictures/funny-picture-3.png',
-    './pictures/funny-picture-4.jpg',
-]
+    "./pictures/funny-picture.png",
+    "./pictures/funny-picture-2.jpg",
+    "./pictures/funny-picture-3.png",
+    "./pictures/funny-picture-4.jpg",
+];
 
-const carouselImg = document.querySelector('.carousel-img');
+const carouselImg = document.querySelector(".carousel-img");
 
-buttonOne.addEventListener('click', () => {
-    carouselImg.src = pictures[0]
-    text.textContent = 'What!?'
-})
+buttonOne.addEventListener("click", () => {
+    carouselImg.src = pictures[0];
+    text.textContent = "What!?";
+});
 
-buttonTwo.addEventListener('click', () => {
-    carouselImg.src = pictures[1]
-    text.textContent = 'How I cool'
-})
+buttonTwo.addEventListener("click", () => {
+    carouselImg.src = pictures[1];
+    text.textContent = "How I cool";
+});
 
-buttonThree.addEventListener('click', () =>{
-    carouselImg.src = pictures[2]
-    text.textContent = 'I`m always'
-})
+buttonThree.addEventListener("click", () => {
+    carouselImg.src = pictures[2];
+    text.textContent = "I`m always";
+});
 
-buttonFour.addEventListener('click', () =>{
-    carouselImg.src = pictures[3]
-    text.textContent = 'smart smart'
-})
- */
+buttonFour.addEventListener("click", () => {
+    carouselImg.src = pictures[3];
+    text.textContent = "smart smart";
+});
